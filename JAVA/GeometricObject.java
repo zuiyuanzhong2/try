68812c7c34afbcfdd399e4d50a97ee7058dbe3f8
zuiyuanzhong2 <1253754007@qq.com>
2021-10-18 17:00:57 +0800

    h

---
+++ JAVA/GeometricObject.java
@@ -0,0 +1,7 @@
+public abstract class GeometricObject{
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    
+}
--- JAVA/W6P1.java
+++
@@ -1,6 +0,0 @@
-public class W6P1{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
-    }
-}
