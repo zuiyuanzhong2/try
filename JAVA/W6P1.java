b81f129d906048047202b9f4d4c0838643fe4502
zuiyuanzhong2 <1253754007@qq.com>
2021-10-20 11:41:01 +0800

    hh

--- JAVA/W6P1.java
+++ JAVA/W6P1.java
@@ -1,7 +1,7 @@
-public abstract class GeometricObject{
+public abstract class W6P1{
     private String color = "white";
     private boolean filled;
     private java.util.Date dateCreated;
-
+    
     
 }
