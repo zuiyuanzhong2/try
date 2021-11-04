9fe14ce8693deee91b908e41c68e26c52031aa97
zuiyuanzhong2 <1253754007@qq.com>
2021-11-04 08:28:50 +0800

    h

---
+++ JAVA/fk.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class fk extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        Scene scene=
+    }
+}
