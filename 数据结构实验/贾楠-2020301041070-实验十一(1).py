db5b7027fbce5dd076f91cf21011fa17370ed187
zuiyuanzhong2 <1253754007@qq.com>
2021-11-15 22:45:38 +0800

    rty

---
+++ 数据结构实验/贾楠-2020301041070-实验十一(1).py
@@ -0,0 +1,70 @@
+#类名称:BinaryTreeNode
+#类说明:定义一个二叉树的结点
+#类释义:分别有左孩子 LeftChild，右孩子 RightChild 和数据 data
+class BinaryTreeNode(object):
+    def __init__(self):
+        self.data = '#'
+        self.LeftChild = None 
+        self.RightChild = None
+class TreeState(object):
+    def __init__(self,BinaryTreeNode,VisitedFlag):
+        self.BinaryTreeNode = BinaryTreeNode
+        self.VisitedFlag = VisitedFlag
+
+# 类说明:定义一个二叉树
+class BinaryTreeNonRecursive(BinaryTreeNode): 
+# 创建二叉树的函数
+    def CreateBinaryTree(self, Root):
+        data = input('->')
+        if data == '#':
+            Root = None
+        else:
+            Root.data = data
+            Root.LeftChild = BinaryTreeNode()
+            self.CreateBinaryTree(Root.LeftChild)
+            Root.RightChild = BinaryTreeNode() 
+            self.CreateBinaryTree(Root.RightChild)
+
+#先序遍历非递归算法
+    def PreOrderNonRecursive(self, Root):
+        StackTreeNode = []
+        tTreeNode = Root
+        StackTreeNode.append(tTreeNode)
+        while len(StackTreeNode) >0:
+            #将栈顶元素弹出并访问
+            tTreeNode=StackTreeNode.pop() 
+            self.VisitBinaryTreeNode(tTreeNode)
+            #若tTreeNode有右孩子，就将其入栈
+            if tTreeNode.RightChild != "#"and  tTreeNode.RightChild  is not None:
+                StackTreeNode.append(tTreeNode.RightChild)
+            #若tTreeNode有左孩子，也将其入栈
+            if tTreeNode.LeftChild != "#"and tTreeNode.LeftChild != None:
+                StackTreeNode.append(tTreeNode.LeftChild)
+        
+
+    def VisitBinaryTreeNode(self, BinaryTreeNode):
+        # 值为#的结点代表空结点
+        if BinaryTreeNode.data != '#':
+            print(BinaryTreeNode.data, end="")
+
+#非递归遍历二叉树
+if __name__ =='__main__':
+    btnrn = BinaryTreeNode()
+    btrn = BinaryTreeNonRecursive()
+    #创建一棵二叉树
+    print("******************非递归先序遍历二叉树*****************")
+    print('创建一棵二叉树\n')
+    print(' A')
+    print(' / \\')
+    print(' B C')
+    print(' / \\ / \\')
+    print(' D E F G')
+    print(' / \\/')
+    print('H IJ')
+    print('A B D H # # # E # I # # C F J # # # G # # ')
+    print('请按上述序列输入二叉树中各结点的值(#表示空结点)，\
+    每输入一个值按回车换行:')
+    btrn.CreateBinaryTree(btnrn)
+    print ('对二叉树进行非递归前序遍历:')
+    #前序遍历二叉树
+    btrn.PreOrderNonRecursive(btnrn)
---
+++ 数据结构实验/贾楠-2020301041070-实验十一（2）.py
@@ -0,0 +1,178 @@
+import random
+
+#生成正序、逆序和随机序列，范围为 1 到 500
+
+positiveseq=[i for i in range(1,501)]
+invertedseq=[i for i in range(500,0,-1)]
+randomseq=random.sample(positiveseq,500)
+#设置三个数据文件的存储路径
+#'.'为当前目录
+directory='./'
+#将正序序列数据存入文件 positivesequence.txt 中
+r ="\n".join(str(i) for i in positiveseq)
+with open(directory+'positivesequence.txt','a') as wr: 
+    wr.write(r) 
+#将逆序序列数据存入文件 invertedsequence.txt 中 
+r ="\n".join(str(i) for i in invertedseq)
+with open(directory+'invertedsequence.txt','a') as wr:
+    wr.write(r)
+#将随机序列数据存入文件 randomsequence.txt 中
+r ="\n".join(str(i) for i in randomseq)
+with open(directory+'randomsequence.txt','a') as wr: 
+    wr.write(r)
+
+
+#导入模块 time，用来获取当前时间
+#################################################################
+import time
+#################################################################
+#类名称:ListItem
+#类说明:定义顺序表中的一个节点
+#类释义:定义顺序表中的一个节点
+################################################################# 
+class ListItem(object):
+    def __init__(self ,key,value):
+        self.key = key
+        self.value = value 
+#类名称:SSequenceList
+#类说明:定义一个顺序表 Array
+#类释义:创建一个顺序表 Array，并对其执行相关操作
+#################################################################
+class SSequenceList(object):
+##################################### 
+#初始化顺序表函数
+##################################### 
+    def __init__(self):
+        self.SeqList=[] 
+    #####################################
+    #创建链表函数
+    ##################################### 
+    def CreateSequenceListByInput_Seq(self,seq):
+        self.SeqList.append(ListItem(int(0), 0))
+        with open('./'+seq+'.txt', 'r') as f:
+            data = f.readlines()
+        i=1
+        for line in data:
+            self.SeqList.append(ListItem(int(line),i))
+            i=i+1
+    ############################# 
+    #冒泡排序
+    ############################# 
+    def BubbleSort(self):
+        SeqListLen = len(self.SeqList)
+        for i in range(1,SeqListLen-1):
+            for j in range(1,SeqListLen-i):
+                if self.SeqList[j+1].key < self.SeqList[j].key:
+                    self.SeqList[0].key = self.SeqList[j].key
+                    self.SeqList[j].key = self.SeqList[j+1].key
+                    self.SeqList[j+1].key = self.SeqList[0].key
+    #############################
+    #快速排序的一趟分区
+    #############################
+    def AdjustPartition(self,low,high):
+        left=low
+        right=high
+        self.SeqList[0].key = self.SeqList[left].key
+        while left < right:
+            while left < right and  self.SeqList[right].key >= self.SeqList[0].key:
+                right = right-1
+            self.SeqList[left].key = self.SeqList[right].key
+            while left < right and self.SeqList[left].key <= self.SeqList[0].key:
+                left = left+1
+            self.SeqList[right].key = self.SeqList[left].key
+        self.SeqList[left].key= self.SeqList[0].key
+        return left
+    #############################
+    #快速排序
+    ############################# 
+    def QuickSort(self,low,high):
+        if low<high:
+            pivot = self.AdjustPartition(low,high)
+            self.QuickSort(low,pivot-1)
+            self.QuickSort(pivot+1,high)
+    #################################
+    #输出函数
+    #################################
+    def PrintOut(self):
+        print("***************************************************")
+        print("\n(1)对正序序列进行冒泡排序和快速排序所花的时间:")
+        try:
+            self.__init__() 
+        except:
+            print("顺序表 Array 初始化失败") 
+        self.CreateSequenceListByInput_Seq('positivesequence') 
+        t1=time.time()
+        (○13 ) #调用冒泡排序算法
+        t2=time.time()
+        tm=t2-t1 
+        print(' 冒泡排序所花的时间为:',tm)
+        try:
+            self.__init__()
+        except:
+            print("顺序表 Array 初始化失败")
+        self.CreateSequenceListByInput_Seq('positivesequence')
+        Len=len(self.SeqList)
+        t1=time.time()
+        (○14 ) #调用快速排序算法
+        t2=time.time() tk=t2-t1
+        print(' 快速排序所花的时间为:',tk) 
+        if tm > tk:
+            print(' 结论:快速排序所花时间更短')
+        else:
+            print(' 结论:冒泡排序所花时间更短')
+        print("\n(2)对逆序序列进行冒泡排序和快速排序所花的时间:")
+        try:
+            self.__init__() 
+        except:
+            print("顺序表 Array 初始化失败")
+        self.CreateSequenceListByInput_Seq('invertedsequence')
+        t1=time.time()
+        (○13 ) #调用冒泡排序算法
+        t2=time.time()
+        tm=t2-t1
+        print(' 冒泡排序所花的时间为:',tm)
+        try:
+            self.__init__()
+        except:
+            print("顺序表 Array 初始化失败")
+        self.CreateSequenceListByInput_Seq('invertedsequence')
+        Len=len(self.SeqList)
+        t1=time.time()
+        (○14 ) #调用快速排序算法
+        t2=time.time()
+        tk=t2-t1
+
+        print(' 快速排序所花的时间为:',tk)
+        if tm > tk:
+            print(' 结论:快速排序所花时间更短') 
+        else:
+            print(' 结论:冒泡排序所花时间更短')
+        print("\n(3)对随机序列进行冒泡排序和快速排序所花的时间:")
+        try:
+            self.__init__()
+        except:
+            print("顺序表 Array 初始化失败")
+        self.CreateSequenceListByInput_Seq('randomsequence')
+        t1=time.time()
+        (○13 ) #调用冒泡排序算法
+        t2=time.time()
+        tm=t2-t1
+        print(' 冒泡排序所花的时间为:',tm)
+        try:
+            self.__init__()
+        except:
+            print("顺序表 Array 初始化失败")
+        self.CreateSequenceListByInput_Seq('randomsequence')
+        Len=len(self.SeqList)
+        t1=time.time()
+        (○14 ) #调用快速排序算法
+        t2=time.time()
+        tk=t2-t1
+        print(' 快速排序所花的时间为:',tk)
+        if tm > tk:
+            print(' 结论:快速排序所花时间更短')
+        else:
+            print(' 结论:冒泡排序所花时间更短') 
+if __name__ =='__main__':
+ Array=SSequenceList()
+ Array.PrintOut()
